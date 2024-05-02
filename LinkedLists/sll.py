e48bb6987098c25315bc3388137a95e78d50cce7
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-05-02 06:39:30 +0100

    method descriptions

--- LinkedLists/sll.py
+++ LinkedLists/sll.py
@@ -1,5 +1,5 @@
 '''
-This library seamlessly allows you to create singlely linked lists!!!
+This library seamlessly allows you to create singly linked lists!!!
 I have made sure to make this library and its methods easy to understand by adding
 comment where I can. I hope you find ease in its use!
 
@@ -9,7 +9,7 @@ comment where I can. I hope you find ease in its use!
         ==============================
 '''
 
-# node for building a singley linked list
+# node for building a singly linked list
 class Node:
     def __init__(self, data):
         self.data = data
@@ -17,11 +17,20 @@ class Node:
 
 # linked list class
 class LinkedList:
+    '''
+    This linked list class contains a variety of standard list manipulation/interaction methods
+    to suit your needs and make the idea of creating a linked list a simple reality!
+    '''
     def __init__(self):
         self.head = None
 
     # time complextity O(1)
-    def add_node_beginning(self, data):
+    def prepend(self, data):
+        '''
+        This method adds a new node to the beginning of the linked list.
+        If there is no node at the beginning of the list, then the new node
+        will become the first node in the list.
+        '''
         new_node = Node(data)
         if self.head == None:
             self.head = new_node
@@ -31,6 +40,11 @@ class LinkedList:
 
     # time complexity O(n)
     def add_node_end(self, data):
+        '''
+        This method adds a node to the end of the  linked list.
+        If there is no node in the list, then the new node will become
+        the first node in the list.
+        '''
         new_node = Node(data)
         if self.head == None:
             self.head = new_node
