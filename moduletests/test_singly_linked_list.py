86fb49f0f7c6ec02c89fdcb6cead6686f88e443f
Joshua Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-05-08 23:23:38 +0100

    Updated unit tests

--- moduletests/test_singly_linked_list.py
+++ moduletests/test_singly_linked_list.py
@@ -1,45 +1,59 @@
 '''
-unit test
+unit testing
 '''
 
 import unittest
-from Libraries.linkedLists.ll import SinglyLinkedList
+from linkedLists.ll import SinglyLinkedList
 
 
-# Unit tests to verify functionality of the linked list library
+# Unit tests to verify the functionality of the linked list library
 
 
-class TestExamples(unittest.TestCase):
-    '''
-    unit testing for LinkedLists module
-    '''
-    def test_delete_data_from_list(self):
-        '''
-        test for delete method
-        '''
-        # arrange
-        sll = SinglyLinkedList()
-        for i in range(10):
-            sll.append(i)
-        # act
-        result = sll.delete(5)
-        # assert
-        self.assertEqual(result, 5)
+class TestLinkedList(unittest.TestCase):
 
+    def setUp(self):
+        # ArrangeAll (to avoid clunky repeats)
+        self.sll = SinglyLinkedList()
+        for i in range(5):
+            self.sll.append(i)
 
     def test_prepend_data_to_list(self):
         '''
         test for prepend method
         '''
         # arrange
-        sll = SinglyLinkedList()
-        for i in range(5):
-            sll.append(i)
-        sll.prepend('99')
+        ll = self.sll
+        ll.prepend('99')
         # act
-        result = sll.head.data
+        result = ll.head.data
         # assert
         self.assertEqual(result, '99')
+
+    def test_append_method(self):
+        '''
+        test for append method
+        '''
+        # Arrange
+        ll = self.sll
+        # Act
+        ll.append(99)
+        last = ll.head
+        while last.next is not None:
+            last = last.next
+        result = last.data
+        # Assert
+        self.assertEqual(result, 99)
+
+    def test_delete_method(self):
+        '''
+        test for delete method
+        '''
+        # Arrange
+        ll = self.sll
+        # Act
+        result = ll.delete(3)
+        # Assert
+        self.assertEqual(result, 3)
 
 
 if __name__ == "__main__":
